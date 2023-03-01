ca15e0e1892a182ea1816a0a3655956cb91446b2
BitPT <phamtobit123@gmail.com>
2023-03-01 17:24:25 +0700

    tekup-project

---
+++ my-project/src/Data/TekupData.js
@@ -0,0 +1,27 @@
+export const TekupData = [
+    {
+        id: 1,
+        img: "https://tekup.vn/wp-content/uploads/2022/09/image-1.png",
+        title: "TOI 3D Customize E-commerce",
+    },
+    {
+        id: 2,
+        img: "https://tekup.vn/wp-content/uploads/2022/09/image.png",
+        title: "E-learning-Internal training platform",
+    },
+    {
+        id: 3,
+        img: "https://tekup.vn/wp-content/uploads/2022/08/summer21-thumbnail.png",
+        title: "Summer 21 Cosmetic E-commerce Platform",
+    },
+    {
+        id: 4,
+        img: "https://tekup.vn/wp-content/uploads/2022/08/kiva-thumbnail.png",
+        title: "Kiva - Ambition to digital transformation in the brokerage assiduity",
+    },
+    {
+        id: 5,
+        img: "https://tekup.vn/wp-content/uploads/2022/08/boxgo-thumbnail.png",
+        title: "Boxgo - Professional Warehouse Management",
+    },
+]
--- my-project/src/tekup-project/Home.js
+++ my-project/src/tekup-project/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import "./TekupStyle.css";
+import { TekupData } from "../Data/TekupData.js";
 
-const Home = () => {
-    return (
-        <div>
-            
+function Home() {
+  return (
+    <div>
+      <div className="tekup-menu">
+        <div className="all">All</div>
+        <div className="manpower-supply">Manpower Supply</div>
+        <div className="maw">Mobile App / Websites</div>
+        <div className="design">UI/UX Design</div>
+      </div>
+      {TekupData.map((item, index) => (
+        <TekupProject
+          key={item.id}
+          img={item.img}
+          title={item.title}
+        ></TekupProject>
+      ))}
+    </div>
+  );
+}
+function TekupProject(props) {
+  return (
+    <div className="main-item">
+      <div className="tekup-item">
+        <div className="image-item">
+          <img src={props.img} alt="" />
         </div>
-    );
-};
-
+        <div className="content">
+          <p className="title">{props.title}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Home;
